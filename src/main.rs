345e5cfbd18a417e364d5e0e7418e704107247cf
logan-keede <68557630+logan-keede@users.noreply.github.com>
2024-11-03 09:46:23 +0530

    cat-file attempt

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::path::PathBuf;
+use std::io::Read;
+extern crate flate2;
+use flate2::read::ZlibDecoder;
+
+fn read_cat_file(sha: &String){
+
+    let sha_file_path = PathBuf::from(format!(".git/objects/{}/{}", &sha[0..2], &sha[2..]));
+    let sha_file = fs::File::open(&sha_file_path);
+    let mut decoder= ZlibDecoder::new(sha_file.unwrap());
+    let mut decompressed_data = String::new();
+    let _ = decoder.read_to_string(&mut decompressed_data);
+    println!("{}", decompressed_data);
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,7 +29,23 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
-    } else {
+    }
+    else if args[1] == "cat-file" {
+        if let Some(pos) = args.iter().position(|arg| arg == "-p") {
+            // Get the argument following "-p", if it exists
+            if let Some(file) = args.get(pos + 1) {
+                read_cat_file(file);
+            } else {
+                eprintln!("Error: No value provided for -p flag");
+            }
+        // let mut decoder= ZlibDecoder::new();
+        }   else { println!("-p flag not found"); }
+    }
+
+    else {
         println!("unknown command: {}", args[1])
     }
+
+
+
 }
